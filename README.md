566e85ecc0612aae3ba9f19da3e747c12484db00
Tony Giamanco <tony1427@gmail.com>
2019-08-26 12:57:27 -0500

    Refactored to use example to allow the request can grow or shrink depending on what the user is searching for and added more info to the readme

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # kotlin-agent-api
 Kotlin project
 
+http://localhost:8080/search
 
 # Testing 
+* The request is a post request. 
+* In postman, add the json request in the body. 
+* Click Send. If you have your database set up correctly, you should see one result.
+* You can add more search parameters as need to refine the search
+
+### Request 
+```json
+{
+	"state":"mo",
+	"brand":"united",
+	"msaRegion": "St. Louis",
+	"postalCode":"63122"
+}
+```
+
+##### Additional Params
+```java 
+     name
+     phone 
+     address1 
+     address2 
+     city
+     state
+     brand
+     postalCode
+     commonOwner
+     url
+     msaRegion
+     country
+```
+    
--- src/main/kotlin/io/agent/controller/AgentController.kt
+++ src/main/kotlin/io/agent/controller/AgentController.kt
@@ -2,20 +2,22 @@ package io.agent.controller
 
 import io.agent.entity.Agent
 import io.agent.repository.AgentRepository
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.data.domain.Example
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.data.domain.ExampleMatcher
 import org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.startsWith
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @RestController
 class AgentController(private val agentRepository: AgentRepository) {
     companion object{
-         val AGENT_MATCHER = ExampleMatcher.matching().withIgnoreCase().withMatcher("postalCode", startsWith())
+        val AGENT_MATCHER = ExampleMatcher.matching().withIgnoreCase().withMatcher("postalCode", startsWith())
     }
 
-    @GetMapping
-    fun search(): List <Agent> {
-        return agentRepository.findByState("TX")
+    @PostMapping("search")
+    fun search(@RequestBody agent: Agent): Iterable <Agent> {
+        return agentRepository.findAll(Example.of(agent, AGENT_MATCHER))
     }
 }
 
